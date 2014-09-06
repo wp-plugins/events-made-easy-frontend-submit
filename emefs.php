a0375627725144d63fafc863026cd7518b448e18
liedekef <liedekef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-06 13:27:29 +0000

    release 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/events-made-easy-frontend-submit/trunk@983018 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- emefs.php
+++ emefs.php
@@ -4,7 +4,7 @@ Plugin Name: Events Made Easy Frontend Submit
 Plugin URI: http://www.e-dynamics.be/wordpress
 Description: Displays a form to allow people to enter events for the Event Made Easy plugin on a regular wordpress page.
 Author: Franky Van Liedekerke
-Version: 1.0.0
+Version: 1.0.1
 Author URI: http://www.e-dynamics.be/wordpress
 License: GNU General Public License
 */
